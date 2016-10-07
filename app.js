8dab6723e8fed521c7582f1c51e730ea782ea734
Monika Zarako-Dobrowner <Monika@Monikas-iMac.local>
2016-10-07 09:14:41 -0700

    general functioning, autumn styling

---
+++ app.js
@@ -0,0 +1,40 @@
+
+
+$(document).ready(function() {
+
+	searchKeyword("kittens");
+
+	$('form').submit(function(e) {
+	e.preventDefault();
+	var userInput = $('#userInput').val();
+	console.log(userInput);
+	searchKeyword(userInput);
+	});
+});
+
+
+function searchKeyword(wordToFind) {
+	var para = {
+		part: 'snippet',
+		key: 'AIzaSyDyKbpXifTaQUi1yhKKZErJeixCw_fT_Qs',
+		q: wordToFind,
+		maxResults: 24,
+		type: 'video',
+	};
+	console.log(para);
+	var Yurl = "https://www.googleapis.com/youtube/v3/search";
+	$.getJSON(Yurl, para, function(data) {
+		displayResults(data.items);
+	})
+}
+
+function displayResults(results) {
+	$('main').children().remove();
+	var elements = "";
+	$.each(results, function(index, value) {
+		elements += '<table><tr><td><a href="https://www.youtube.com/watch?v=' + value.id.videoId + '" target="_blank"><img src="' + 
+		value.snippet.thumbnails.medium.url + '"></td></tr><tr><td class="video-link"><a href="https://www.youtube.com/watch?v=' + 
+		value.id.videoId + '" target="_blank"><p class="video-title">' + value.snippet.title + '</p></td></tr></table>';
+	});
+	$('main').append(elements);
+}
